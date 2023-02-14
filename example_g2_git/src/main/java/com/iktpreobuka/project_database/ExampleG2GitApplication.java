f3428671e7f233f3474a77ef5c787e8a0a4b2484
pavelbohus <pavelbohus>
2023-02-14 20:43:37 +0100

    Moj prvi komit

---
+++ example_g2_git/src/main/java/com/iktpreobuka/project_database/ExampleG2GitApplication.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.project_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleG2GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleG2GitApplication.class, args);
+	}
+
+}
